5c42d44eafb0e4f0988b233cd182e0ab0a89f27e
Vlad Labanov <labanovvlad@gpu.bioinf.fbb.msu.ru>
2025-08-24 09:57:34 +0000

    TISM annotated

--- tangermeme/tism.py
+++ tangermeme/tism.py
@@ -1,7 +1,6 @@
 # tism.py
 
 import torch
-import itertools
 
 from tqdm import trange
 
@@ -16,73 +15,69 @@ def _predict_grad(
     device="cuda",
     verbose=False,
 ):
-    """Make batched predictions in a memory-efficient manner.
+    """An internal function for calculating the TISM batched gradients in
+    a memory-efficient manner.
 
-    This function will take a PyTorch model and make predictions from it using
-    the forward function, with optional additional arguments to the model. The
-    additional arguments must have the same batch size as the examples, and the
-    i-th example will be given to the model with the i-th index of each
-    additional argument.
+    This function, which is meant to be used for TISM, will take a PyTorch
+    model and calculate gradients from it using forward function of the model
+    and Pytorch autograd. The optional additional arguments of the model must
+    have the same batch size as the examples, and the i-th example will be
+    given to the model with the i-th index of each additional argument.
 
     Before starting predictions, the model is moved to the specified device. As
     predictions are being made, each batch is also moved to the specified
-    device and then moved back to the CPU after predictions are made. Each batch
-    is converted to the provided dtype if provided, keeping the original blob of
-    examples in the original dtype. These features allow the function to work on
-    massive data sets that do not fit in GPU memory. For example, the original
-    sequences can be kept as 8-bit integers for compression and each batch will
-    be upcast to the desired precision. If a single batch does not fit in memory,
-    try lowering the batch size.
+    device and then moved back to the CPU after gradients are calculated. Each
+    batch is converted to the provided dtype if provided, keeping the original
+    blob of examples in the original dtype. These features allow the function
+    to work on massive data sets that do not fit in GPU memory. For example,
+    the original sequences can be kept as 8-bit integers for compression and
+    each batch will be upcast to the desired precision. If a single batch does
+    not fit in memory, try lowering the batch size.
 
 
     Parameters
     ----------
     model: torch.nn.Module
-            The PyTorch model to use to make predictions.
+            The PyTorch model to use to calculate gradients.
 
-    X: torch.tensor, shape=(-1, len(alphabet), length)
-            A one-hot encoded set of sequences to make predictions for.
+    X: torch.Tensor, shape=(-1, len(alphabet), length)
+            A one-hot encoded set of sequences to calculate gradients for.
 
     args: tuple or list or None, optional
             An optional set of additional arguments to pass into the model. If
-            provided, each element in the tuple or list is one input to the model
-            and the element must be formatted to be the same batch size as `X`. If
-            None, no additional arguments are passed into the forward function.
-            Default is None.
-
-    func: function or None, optional
-            A function to apply to a batch of predictions after they have been made.
-            If None, do nothing to them. Default is None.
+            provided, each element in the tuple or list is one input to the
+            model and the element must be formatted to be the same batch size
+            as `X`. If None, no additional arguments are passed into the
+            forward function. Default is None.
 
     batch_size: int, optional
-            The number of examples to make predictions for at a time. Default is 32.
+            The number of examples to calculate gradients for at a time.
+            Default is 32.
 
     target: int or slice or None
-            If the user wants to subset to only some targets when calculating the
-            average attribution across targets. If None, use all.
+            If the user wants to subset to only some targets when calculating
+            the average gradient across targets. If None, use all.
 
     dtype: str or torch.dtype or None, optional
-            The dtype to use with mixed precision autocasting. If None, use the dtype of
-            the *model*. This allows you to use int8 to represent large data sets and
-            only convert batches to the higher precision, saving memory. Defailt is None.
+            The dtype to use with mixed precision autocasting. If None, use
+            the dtype of the *model*. This allows you to use int8 to represent
+            large data sets and only convert batches to the higher precision,
+            saving memory. Defailt is None.
 
     device: str or torch.device, optional
-            The device to move the model and batches to when making predictions. If
-            set to 'cuda' without a GPU, this function will crash and must be set
-            to 'cpu'. Default is 'cuda'.
+            The device to move the model and batches to when calculating
+            gradients. If set to 'cuda' without a GPU, this function will
+            crash and must be set to 'cpu'. Default is 'cuda'.
 
     verbose: bool, optional
-            Whether to display a progress bar during predictions. Default is False.
+            Whether to display a progress bar during calculating gradients.
+            Default is False.
 
 
     Returns
     -------
-    y: torch.Tensor or list/tuple of torch.Tensors
-            The output from the model for each input example. The precise format
-            is determined by the model. If the model outputs a single tensor,
-            y is a single tensor concatenated across all batches. If the model
-            outputs multiple tensors, y is a list of tensors which are each
-            concatenated across all batches.
+    y_grad: torch.Tensor, shape=(-1, len(alphabet), length)
+            The gradients of the model for each input example.
     """
 
     model = model.to(device).eval()
@@ -105,7 +100,7 @@ def _predict_grad(
 
     ###
 
-    y = []
+    y_grad = []
     with torch.autograd.set_grad_enabled(True):
         batch_size = min(batch_size, X.shape[0])
 
@@ -131,36 +126,38 @@ def _predict_grad(
                     )
 
                 y_target_like = torch.ones_like(y_target)
-                y_grad = (
+                y_grad_ = (
                     torch.autograd.grad(y_target, X_, grad_outputs=y_target_like)[0]
                     .detach()
                     .cpu()
                 )
 
-            y.append(y_grad)
+            y_grad.append(y_grad_)
 
-    return torch.cat(y)
+    y_grad = torch.cat(y_grad)
+    return y_grad
 
 
 def _attribution_score(y_hat_grad):
     """An internal function for calculating the TISM attributions.
 
     This function, which is meant to be used for TISM, will take in the
-    predictions before and after substitutions and a target -- which can be
-    None -- and return the position-normalized differences. Specifically,
-    for each example, the differences in prediction will be normalized by
-    subtracting out the per-position average, and then averaged across
-    all tasks if target is None.
+    gradients and return TISM attributions.
 
 
     Parameters
     ----------
-    y_hat: torch.Tensor, shape=(-1, len(alphabet), length, n_targets)
-            Model predictions for each example for each substitution.
+    y_hat_grad: torch.Tensor, shape=(-1, (end-start), len(alphabet), length)
+            Model gradients for each example for each substitution.
+
+
+    Returns
+    -------
+    attr: torch.Tensor, shape=(-1, len(alphabet), length)
+            The TISM attributions for each input example.
     """
 
-    y_hat_grad = torch.mean(y_hat_grad, dim=1)
-    attr = y_hat_grad
+    attr = torch.mean(y_hat_grad, dim=1)
     attr -= torch.sum(attr, dim=1, keepdims=True)
 
     return attr
@@ -169,9 +166,9 @@ def _attribution_score(y_hat_grad):
 def _edit_distance_one(X, start, end):
     """An internal function for generating all sequences of edit distance 1
 
-    This internal function, which is meant to be used for ISM, will take in a
-    one-hot encoded sequence and return all sequences that have an edit distance
-    of one.
+    This internal function, which is meant to be used for TISM, will take in a
+    one-hot encoded sequence and return all sequences that have an edit
+    distance of one.
 
 
     Parameters
@@ -189,9 +186,9 @@ def _edit_distance_one(X, start, end):
 
     Returns
     -------
-    X_: torch.Tensor, shape=(length*len(alphabet), len(alphabet), length)
-            All one-hot encoded sequences that have an edit distance of 1 from the
-            original sequence.
+    X_: torch.Tensor, shape=(length, len(alphabet), length)
+            All one-hot encoded sequences that have an edit distance of 1 from
+            the original sequence.
     """
 
     end = end if end >= 0 else X.shape[-1] + 1 + end
@@ -218,21 +215,19 @@ def tism(
     device="cuda",
     verbose=False,
 ):
-    """Performs in-silico saturation mutagenesis on a set of sequences.
+    """Performs Taylor in-silico saturation mutagenesis on a set of sequences.
 
-    This function will perform in-silico saturation mutagenesis on a set of
-    sequences and return the predictions on the original sequences and each
-    of the sequences with an edit distance of one on them.
+    This function will perform Taylor in-silico saturation mutagenesis on a
+    set of sequences and return the gradients for the original sequences and
+    each of the sequences with an edit distance of one on them.
 
-    By default, this function will aggregate these predictions into an
-    attribution value. This aggregation involves taking the Euclidean distance
-    between the predictions before and after the substitutions and Z-score
-    normalizing them across the entire example. However, this assumes that
-    the model returns only a single tensor. This tensor can have multiple
-    outputs, e.g., be of shape (batch_size, n_targets) where n_targets > 1,
-    but the model cannot return multiple tensors.
+    By default, this function will aggregate these gradients into an
+    attribution value. However, this assumes that the model returns only a
+    single tensor. This tensor can have multiple outputs, e.g., be of shape
+    (batch_size, n_targets) where n_targets > 1, but the model cannot return
+    multiple tensors.
 
-    If you simply want the predictions before and after the substitutions
+    If you simply want the gradients after the substitutions
     without the method turning those into attributions because, perhaps, you
     want to define your own aggregation method, you can use `raw_outputs=True`.
 
@@ -240,7 +235,7 @@ def tism(
     Parameters
     ----------
     model: torch.nn.Module
-            The PyTorch model to use to make predictions.
+            The PyTorch model to use to calculate gradients.
 
     X: torch.tensor, shape=(-1, len(alphabet), length)
             A set of one-hot encoded sequences to calculate attribution values
@@ -248,63 +243,65 @@ def tism(
 
     args: tuple or None, optional
             An optional set of additional arguments to pass into the model. If
-            provided, each element in the tuple or list is one input to the model
-            and the element must be formatted to be the same batch size as `X`. If
-            None, no additional arguments are passed into the forward function.
-            Default is None.
+            provided, each element in the tuple or list is one input to the
+            model and the element must be formatted to be the same batch size
+            as `X`. If None, no additional arguments are passed into the
+            forward function. Default is None.
 
     start: int, optional
             The start of where to begin making perturbations to the sequence.
             Default is 0.
 
     end: int, optional
-            The end of where to to make perturbations to the sequence. If end is
-            positive, it is non-inclusive. If end is negative, it is inclusive.
-            Default is -1, meaning the entire sequence.
+            The end of where to to make perturbations to the sequence. If end
+            is positive, it is non-inclusive. If end is negative, it is
+            inclusive. Default is -1, meaning the entire sequence.
 
     batch_size: int, optional
-            The number of examples to make predictions for at a time. Default is 32.
+            The number of examples to calculate gradients for at a time.
+            Default is 32.
 
     target: int or slice or None, optional
             Whether to focus on a single output/slice of outputs from the model
-            when calculating attributions rather than the entire set of outputs.
-            If None, use all targets when calculating distances. Default is None.
+            when calculating attributions rather than the entire set of
+            outputs. If None, use all targets when calculating distances.
+            Default is None.
 
     hypothetical: bool, optional
             Whether to return attributions for all possible characters at each
-            position or only for the character that is actually at the sequence.
-            Only matters when `raw_outputs=False`. Default is False.
+            position or only for the character that is actually at the
+            sequence. Only matters when `raw_outputs=False`. Default is False.
 
     raw_outputs: bool, optional
             Whether to return the raw outputs from the method -- in this case,
-            the predictions from the reference sequence and from each of the
-            perturbations -- or the processed attribution values. Default is False.
+            the gradients from each of the perturbations -- or the processed
+            attribution values. Default is False.
 
     dtype: str or torch.dtype or None, optional
-            The dtype to use with mixed precision autocasting. If None, use the dtype of
-            the *model*. This allows you to use int8 to represent large data sets and
-            only convert batches to the higher precision, saving memory. Defailt is None.
+            The dtype to use with mixed precision autocasting. If None,
+            use the dtype of the *model*. This allows you to use int8 to
+            represent large data sets and only convert batches to the higher
+            precision, saving memory. Defailt is None.
 
     device: str or torch.device, optional
-            The device to move the model and batches to when making predictions. If
-            set to 'cuda' without a GPU, this function will crash and must be set
-            to 'cpu'. Default is 'cuda'.
+            The device to move the model and batches to when calculating
+            gradients. If set to 'cuda' without a GPU, this function will
+            crash and must be set to 'cpu'. Default is 'cuda'.
 
     verbose: bool, optional
-            Whether to display a progress bar during predictions. Default is False.
+            Whether to display a progress bar during calculating gradients.
+            Default is False.
 
 
     Returns
     -------
-    attr: torch.Tensor
-            Processed attribution values as the z-score normalized difference
-            between the difference in predictions for the original sequence and
-            the perturbed sequences.
+    attr: torch.Tensor, shape=(-1, len(alphabet), length)
+            The TISM attributions for each of the input sequences.
 
     -- or, if raw_outputs=True --
 
-    y_hat: torch.Tensor or list/tuple of torch.Tensors
-            The outputs from the model for each of the perturbed sequences.
+    y_hat_grad: torch.Tensor, shape=(-1, (end-start), len(alphabet), length)
+            The gradients from the model for each of the perturbed sequences.
     """
 
     if end < 0:
@@ -338,9 +335,9 @@ def tism(
         X.shape[0], end - start, *y_hat_grad_.shape[1:]
     )
 
-    if raw_outputs == False:
+    if raw_outputs is False:
         attr = _attribution_score(y_hat_grad)
 
-        return X * attr if hypothetical == False else attr
+        return X * attr if hypothetical is False else attr
 
     return y_hat_grad
